bec2b340e030bd0c6a9340a7c4c5ac8eab4d494e
Kevin Clark <kclark@gamingproject.net>
2022-04-14 20:22:11 -0600

    skeleton

---
+++ examples/wandering_screeps/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func runLoop() {
+	// runLoop() gets executed every tick
+	return
+}
+
+func main() {
+	fmt.Println("Hello, WebAssembly!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kevclarx/screeps-arena-go
+
+go 1.18
